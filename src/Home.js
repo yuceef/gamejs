0e3728bcffbd4e8b8b2c7ffbb0b16fb35936cf1d
yuceef <yuceef.hamlili@gmail.com>
2018-10-06 12:09:27 +0100

    Description Signed-off-by: yuceef <yuceef.hamlili@gmail.com>

--- src/Home.js
+++ src/Home.js
@@ -34,9 +34,12 @@ class Home extends Component {
           <p>
             Loading <code>GameJs</code> Please wait ...
           </p>
+          <p style={{fontSize: "15px"}}>In this game , we have a table of numbers less than 25 , <br /> the player must select cells whose sum is 25 in a precise duration.</p>
+
           <div className="progress" style={{width: "100%",height: "30px"}}>
             <div className="progress-bar progress-bar-striped bg-theme font-weight-bold" role="progressbar" style={{width: this.state.pourcentage + "%",height: "30px"}} aria-valuenow={this.state.pourcentage} aria-valuemin="0" aria-valuemax="100">{this.state.pourcentage + "%"}</div>
           </div>
+
           {
             (this.state.end)? <button onClick={this.props.handleStart} className="btn btn-info btn-big bg-theme mt-2 col-md-4 font-weight-bold">Start</button>:null
           }
